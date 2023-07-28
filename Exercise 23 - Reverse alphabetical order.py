861ae2e6f678e1107f1419c1f111ee88f4119407
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-28 15:05:45 +0100

    Add files via upload

---
+++ Exercise 23 - Reverse alphabetical order.py
@@ -0,0 +1,17 @@
+#Exercício 23:
+#Elabora um programa que imprima os nomes da equipa
+#de um projeto ordenada por ordem iversa alfabética:
+#Considere os elementos da equipa António, Ana, Teresa
+#Telmo, João, Guilherme e Luís
+
+lista_nomes=['António', 'Ana', 'Teresa', 'Telmo', 'João', 'Guilherme', 'Luís']
+
+list.sort(lista_nomes)
+list.reverse(lista_nomes)
+print (lista_nomes)
+
+
+
+
+
+
